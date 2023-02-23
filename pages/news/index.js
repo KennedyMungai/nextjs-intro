6787d904b1bc7349bcb663a46bd3a1b7ac1f7056
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-23 15:41:20 +0300

    Replaced the anchor tag with the Link tag

--- pages/news/index.js
+++ pages/news/index.js
@@ -6,14 +6,14 @@ const newsPage = () =>
     <h1>The News Page</h1>
     <ul>
       <li>
-        <a href="/news/nextjs-is-a-great-framework">
+        <Link href="/news/nextjs-is-a-great-framework">
           NextJS is a great framework
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="/news/something-in-the-way">
+        <Link href="/news/something-in-the-way">
           Something in the way
-        </a>
+        </Link>
       </li>
     </ul>
   </>
