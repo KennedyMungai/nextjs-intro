b2cf3344c9d3cb0dc20b3d5a7ec36513811d1c4e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-23 15:30:06 +0300

    Added an unordered list with two list items to the index js page

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,12 @@
 const newsPage = () =>
 {
-  return <h1>The News Page</h1>;
+  return <>
+    <h1>The News Page</h1>
+    <ul>
+      <li>NextJS is a great framework</li>
+      <li>Something in the way</li>
+    </ul>
+  </>
 };
 
 export default newsPage;
